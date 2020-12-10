8ac7e1469642ac08c44f645af2846ffe2e150ce8
luizcmenezes <lcmscontas@gmail.com>
2020-12-10 09:57:00 -0300

    Implementando a autorização

--- livraria-orig/src/br/com/caelum/livraria/bean/LoginBean.java
+++ livraria-orig/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -20,6 +21,8 @@ public class LoginBean {
 		System.out.println("Teste login" + this.usuario.getEmail());
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if (existe) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		return null;
